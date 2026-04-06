d79158ed2fb2c23ed63aa87e8925e8b35474d976
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-04-07 00:18:42 +0600

    display from data in a table and form error handle

--- src/components/ProductManagement/ProductForm.jsx
+++ src/components/ProductManagement/ProductForm.jsx
@@ -1,21 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ProductForm = () => {
+const ProductForm = ({handleAddProduct}) => {
+
+  const [error, setError] = useState('');
 
   const handleProductSubmit = e => {
     e.preventDefault();
+
     
     const name = e.target.name.value;
     const price = e.target.price.value;
     const quantity = e.target.quantity.value;
     // console.log(name, price, quantity);
 
+
+    //validation
+    if(name.length === 0){
+      setError('please provide a product name')
+      return;
+    }else if(price.length === 0){
+      setError('please provide a price')
+      return;
+    }else if(price <= 0){
+      setError('price can not be nagative')
+      return;
+    }else if(quantity.length === 0 || quantity <= 0){
+      setError('quantity can not be usable')
+      return;
+    }else{
+      setError('')
+    }
+
     const newProduct = {
       name,
       price, 
       quantity
     }
-    console.log(newProduct)
+    // console.log(newProduct)
+    handleAddProduct(newProduct)
   }
 
   return (
@@ -29,6 +51,7 @@ const ProductForm = () => {
         <br />
         <input type="submit" value="Submit" />
       </form>
+      <p style={{color: 'red'}}><small>{error}</small></p>
     </div>
   );
 };
--- src/components/ProductManagement/ProductTable.jsx
+++ src/components/ProductManagement/ProductTable.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-const ProductTable = () => {
+const ProductTable = ({products}) => {
   return (
     <div>
-      
+      <h2>Products: {products.length}</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>Product</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            products.map((product, index) => <tr key={index}>
+            <td>{index + 1}</td>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+            <td>{product.quantity}</td>
+            <td></td>
+          </tr>)
+          }
+        </tbody>
+      </table>
     </div>
   );
 };
